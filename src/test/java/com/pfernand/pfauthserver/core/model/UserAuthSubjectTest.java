6adefd29eefb4ef6b153ba455f9df2421129a3c3
Paulo Fernandes <paulofernandes@192.168.1.133>
2019-04-10 21:54:08 +0100

    Added unit test for enum

---
+++ src/test/java/com/pfernand/pfauthserver/core/model/UserAuthSubjectTest.java
@@ -0,0 +1,36 @@
+package com.pfernand.pfauthserver.core.model;
+
+import org.junit.Test;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.Assert.*;
+
+public class UserAuthSubjectTest {
+
+
+    @Test
+    public void fromStringWhenValidStringReturnsEnum() {
+        // Given
+        final String value = "cst";
+
+        // When
+        UserAuthSubject userAuthSubject = UserAuthSubject.fromString(value);
+
+        // Then
+        assertEquals(UserAuthSubject.CUSTOMER, userAuthSubject);
+    }
+
+    @Test
+    public void fromStringWhenInvalidStringThrowsException() {
+        // Given
+        final String value = "cstInvalid";
+
+        // When
+        // Then
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> UserAuthSubject.fromString(value))
+                .withMessage("No enum with name: " + value);
+    }
+
+}
