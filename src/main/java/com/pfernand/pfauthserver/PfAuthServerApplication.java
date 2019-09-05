b66432845813f3701def512fae71644a0a61d546
Paulo Fernandes <paulofernandes@paulos-mbp-2.mynet>
2019-09-05 22:21:35 +0100

    removed jks for kafka

--- src/main/java/com/pfernand/pfauthserver/PfAuthServerApplication.java
+++ src/main/java/com/pfernand/pfauthserver/PfAuthServerApplication.java
@@ -1,16 +1,11 @@
 package com.pfernand.pfauthserver;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.FileCopyUtils;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -18,16 +13,7 @@ import java.io.IOException;
 @ComponentScan(value = {"com.pfernand.pfauthserver", "com.pfernand.security"})
 public class PfAuthServerApplication {
 
-	private static final String JKS_FILE_NAME = "client.truststore.jks";
-
-	public static void main(String[] args) throws IOException {
-		copyJksToTemp();
+	public static void main(String[] args) {
 		SpringApplication.run(PfAuthServerApplication.class, args);
-	}
-
-	@SuppressFBWarnings
-	private static void copyJksToTemp() throws IOException {
-		FileCopyUtils.copy(new ClassPathResource(JKS_FILE_NAME).getInputStream(),
-				new FileOutputStream("/tmp/" + JKS_FILE_NAME));
 	}
 }
