efac0a150cf705f7c02f99322694aadb4e412110
Paulo Fernandes <paulofernandes@paulos-mbp-2.mynet>
2019-09-08 23:33:29 +0100

    removed commented code

--- src/main/java/com/pfernand/pfauthserver/config/SecurityCredentialsConfiguration.java
+++ src/main/java/com/pfernand/pfauthserver/config/SecurityCredentialsConfiguration.java
@@ -6,12 +6,6 @@ import com.pfernand.pfauthserver.core.security.TokenFactory;
 import com.pfernand.pfauthserver.core.security.JwtUserAuthenticationFilter;
 import com.pfernand.security.JwtConfig;
 import com.pfernand.security.JwtTokenAuthenticationFilter;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
@@ -80,31 +74,5 @@ public class SecurityCredentialsConfiguration extends WebSecurityConfigurerAdapt
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
-//
-//    @Bean
-//    public ServletWebServerFactory servletContainer() {
-//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-////        {
-////            @Override
-////            protected void postProcessContext(Context context) {
-////                SecurityConstraint securityConstraint = new SecurityConstraint();
-////                securityConstraint.setUserConstraint("CONFIDENTIAL");
-////                SecurityCollection collection = new SecurityCollection();
-////                collection.addPattern("/*");
-////                securityConstraint.addCollection(collection);
-////                context.addConstraint(securityConstraint);
-////            }
-////        };
-//        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-//        return tomcat;
-//    }
-//
-//    private Connector redirectConnector() {
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setScheme("http");
-//        connector.setPort(8080);
-//        connector.setSecure(false);
-//        connector.setRedirectPort(8443);
-//        return connector;
-//    }
+
 }
