16fd3bef1c04039c0c7a9f7d62648964eb061f79
Paulo Fernandes <paulofernandes@paulos-mbp-2.mynet>
2019-10-16 22:02:57 +0100

    Added input validation to refresh-token

--- src/main/java/com/pfernand/pfauthserver/adapter/primary/api/RefreshTokenApiController.java
+++ src/main/java/com/pfernand/pfauthserver/adapter/primary/api/RefreshTokenApiController.java
@@ -1,6 +1,7 @@
 package com.pfernand.pfauthserver.adapter.primary.api;
 
 import com.pfernand.pfauthserver.adapter.primary.api.mapper.AuthenticationResponseMapper;
+import com.pfernand.pfauthserver.adapter.primary.api.validation.InputApiValidation;
 import com.pfernand.pfauthserver.core.service.RefreshTokenService;
 import com.pfernand.pfauthserver.port.primary.api.RefreshTokenApi;
 import com.pfernand.pfauthserver.core.security.model.AuthenticationResponse;
@@ -18,11 +19,13 @@ public class RefreshTokenApiController implements RefreshTokenApi<ResponseEntity
 
     private final RefreshTokenService refreshTokenService;
     private final AuthenticationResponseMapper authenticationResponseMapper;
+    private final InputApiValidation inputApiValidation;
 
     @Override
     @PostMapping(value = "/refresh-token", produces = "application/json")
     public ResponseEntity<AuthenticationResponse> refresh(@RequestHeader(value = "X-Refresh-Token", defaultValue = "") String refreshToken) {
-        log.info("POST /auth/refresh-token with params {}", refreshToken);
+        inputApiValidation.validateLength(refreshToken);
+        log.info("POST /auth/refresh-token with params {}", inputApiValidation.encodeForLog(refreshToken));
         return ResponseEntity.ok(authenticationResponseMapper.map(refreshTokenService.refreshToken(refreshToken)));
     }
 
